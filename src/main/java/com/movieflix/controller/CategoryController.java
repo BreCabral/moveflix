f534717ce97520fb356ae34ca9ded8e5d70a4f67
Breno <breno.ara.cabral@gmail.com>
2025-03-28 17:57:00 -0300

    feat: add category mapper and use in controller

--- src/main/java/com/movieflix/controller/CategoryController.java
+++ src/main/java/com/movieflix/controller/CategoryController.java
@@ -1,11 +1,15 @@
 package com.movieflix.controller;
 
+import com.movieflix.controller.request.CategoryRequest;
+import com.movieflix.controller.response.CategoryResponse;
 import com.movieflix.entity.Category;
+import com.movieflix.mapper.CategoryMapper;
 import com.movieflix.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/movieflix/category")
@@ -15,18 +19,24 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping
-    public List<Category> getAllCategories() {
-        return categoryService.findAll();
+    public List<CategoryResponse> getAllCategories() {
+        List<Category> categories = categoryService.findAll();
+        return categories.stream()
+                .map(CategoryMapper::toResponse)
+                .toList();
     }
 
     @PostMapping
-    public Category saveCategory(@RequestBody Category category) {
-        return categoryService.saveCategory(category);
+    public CategoryResponse saveCategory(@RequestBody CategoryRequest request) {
+        Category category = CategoryMapper.toCategory(request);
+        Category saved = categoryService.saveCategory(category);
+        return CategoryMapper.toResponse(saved);
     }
 
     @GetMapping("/{id}")
-    public Category getCategoryById(@PathVariable Long id) {
-        return categoryService.findById(id).orElse(null);
+    public CategoryResponse getCategoryById(@PathVariable Long id) {
+        Optional<Category> category = categoryService.findById(id);
+        return category.map(CategoryMapper::toResponse).orElse(null);
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/com/movieflix/entity/Category.java
+++ src/main/java/com/movieflix/entity/Category.java
@@ -1,11 +1,13 @@
 package com.movieflix.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "category")
 public class Category {
